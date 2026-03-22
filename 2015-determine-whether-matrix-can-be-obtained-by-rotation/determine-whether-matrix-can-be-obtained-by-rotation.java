2b86947563a117e0614ba38801bf8072d83a44bb
Shourya Yadav <148475851+Shourya-Yadav@users.noreply.github.com>
2026-03-22 20:46:56 +0530

    Time: 0 ms (100.00%) | Memory: 43.2 MB (97.73%) - LeetSync

---
+++ 2015-determine-whether-matrix-can-be-obtained-by-rotation/determine-whether-matrix-can-be-obtained-by-rotation.java
@@ -0,0 +1,48 @@
+class Solution {
+    public boolean findRotation(int[][] mat, int[][] target) {
+        for (int i = 0; i < 4; i++) {
+            if (isEqual(mat, target)) return true;
+            rotate(mat);
+        }
+        return false;
+    }
+
+    private void rotate(int[][] mat) {
+        int n = mat.length;
+
+        // Transpose
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                int temp = mat[i][j];
+                mat[i][j] = mat[j][i];
+                mat[j][i] = temp;
+            }
+        }
+
+        // Reverse each row
+        for (int i = 0; i < n; i++) {
+            reverse(mat[i]);
+        }
+    }
+
+    private void reverse(int[] row) {
+        int left = 0, right = row.length - 1;
+        while (left < right) {
+            int temp = row[left];
+            row[left] = row[right];
+            row[right] = temp;
+            left++;
+            right--;
+        }
+    }
+
+    private boolean isEqual(int[][] mat, int[][] target) {
+        int n = mat.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (mat[i][j] != target[i][j]) return false;
+            }
+        }
+        return true;
+    }
+}
